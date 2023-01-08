e2fbc3cbce031a3d712bfe0bcb8097b8a2420181
Ambayonyx <Maurice.Beelen@gmail.com>
2023-01-08 20:30:47 +0100

    Handle file excpetions (#7)
    * Handle failing file read (not existing)
    
    * Handle failing file read (incompatible format)
    
    Tested with .gitignore
    
    * Handle failing file read (failing access rights)

--- data_model.py
+++ data_model.py
@@ -16,17 +16,28 @@ class Model:
 class Model:
     def __init__(self):
         self.df: pd.DataFrame = pd.DataFrame()
+        self.model_status: List[str] = []
         self.days: List[str] = []
 
     def create_model(self, data_file: str) -> Model:
-        self.df = pd.read_csv(
-            data_file,
-            sep=',',
-            header=0,
-            usecols=['date', 'ac_power', 'ac_energy_today'],
-            nrows=10000
-        )
-        self.df[day_column_name] = pd.to_datetime(self.df['date']).dt.date
-        self.df[time_column_name] = pd.to_datetime(self.df['date']).dt.strftime("%H:%M:%S")
+        self.model_status.clear()
+        try:
+            self.df = pd.read_csv(
+                data_file,
+                sep=',',
+                header=0,
+                usecols=['date', 'ac_power', 'ac_energy_today'],
+                nrows=10000
+            )
+            self.df[day_column_name] = pd.to_datetime(self.df['date']).dt.date
+            self.df[time_column_name] = pd.to_datetime(self.df['date']).dt.strftime("%H:%M:%S")
+        except FileNotFoundError as exception:
+            self.model_status.append(f'{str(exception)}')
+        except ValueError as exception:
+            self.model_status.append(f'Incorrect file format? {str(exception)}')
+        except PermissionError as exception:
+            self.model_status.append(f'Are you allowed to access this file? {str(exception)}')
+        except Exception as exception:
+            self.model_status.append(str(exception))
 
         return self
