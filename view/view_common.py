d7e101de96603c80fb68f42d63555826c60f46f2
mbeelen <maurice.beelen@gmail.com>
2023-02-11 19:52:47 +0100

    New 19: Add link to github repository and project page in footer.

--- view/view_common.py
+++ view/view_common.py
@@ -5,6 +5,6 @@ def footer():
     st.markdown('#')
     st.markdown('#')
     st.markdown("""
-    *Note: This page is not a product of Autarco, nor is it affiliated to Autarco. 
-    Autarco holds no responsibility for its content.* 
+    **Note: This page is not a product of Autarco, nor is it affiliated to Autarco. 
+    Autarco holds no responsibility for its content.**
     """)
